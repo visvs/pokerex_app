9a110ebae78484eb60601e689d92453490676293
VioletaVS <viole.vera.salazar@gmail.com>
2022-08-16 09:14:35 -0500

    [config]: initial setup and example of implements redux with connect

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Col } from 'antd';
+import Searcher from './components/Searcher';
+import PokemonList from './components/PokemonList';
+import { getPokemon } from './api';
+import { setPokemons as setPokemonsActions } from './actions';
+import logo from './statics/logo.svg';
+import './App.css';
+
+function App({ pokemons, setPokemons }) {
+  console.log("🚀 ~ file: App.js ~ line 12 ~ App ~ pokemons", pokemons)
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const pokemonsRes = await getPokemon();
+      setPokemons(pokemonsRes);
+    };
+
+    fetchPokemons();
+  }, []);
+
+  return (
+    <div className='App'>
+      <Col span={4} offset={10}>
+        <img src={logo} alt='Pokedux' />
+      </Col>
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
+      <PokemonList pokemons={pokemons} />
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,17 @@
+import PokemonCard from './PokemonCard';
+
+const PokemonList = ({ pokemons }) => {
+  return (
+    <div className='PokemonList'>
+      {pokemons.map((pokemon) => {
+        return <PokemonCard name={pokemon.name} key={pokemon.name} />;
+      })}
+    </div>
+  );
+};
+
+PokemonList.defaultProps = {
+  pokemons: Array(10).fill(''),
+};
+
+export default PokemonList;
