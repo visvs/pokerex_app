da0e061abdc2d3bb28970f1a3ffe82ad10379598
viluvs <viole.vera.salazar@gmail.com>
2022-08-17 10:34:35 -0500

    [change] : omit featuring middleware usage, remove default character

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App.hooks';
 import { pokemonsReducer } from './reducers/pokemons';
 import { Provider } from 'react-redux';
 import {applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
-import { featuring, logger } from './middlewares';
+import { logger } from './middlewares';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //Compose nos sirve para encapsular todos los enhancers y middlewares que se quieran implementar
 //Para poder pasarle como parametro los middlewares creados, como logger en este caso,
 //se debe pasar primero por la funcion applyMiddleware como atributo
-const composeEnhencers = compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(logger, featuring))
+const composeEnhencers = compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(logger))
 
 const store = createStore(pokemonsReducer, composeEnhencers);
 
